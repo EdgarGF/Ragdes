6b9f0941df4ee76646becc51d9001e34137d7096
ragde <fyrogf@gmail.com>
2026-09-03 23:34:52 -0600

    chore:
    Se agrega comoponente y solucionan dependecias con package-log

---
+++ portfolio/src/app/modules/home/home.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-home',
+  styleUrl: './home.css',
+  templateUrl: './home.html',
+})
+export class Home implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
